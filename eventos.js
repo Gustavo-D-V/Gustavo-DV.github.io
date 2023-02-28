4fbae80f545235c3291662482dbd58d237f084fb
Gustavo.Venturino <venturinogustavo@gmail.com>
2023-02-28 20:06:40 -0300

    primera versión

---
+++ eventos.js
@@ -0,0 +1,28 @@
+
+
+
+document.getElementById('ul1datosPer').onclick = function() {
+	window.open( "datos-personales.html","Datos personales","width=400px, height=500px")};
+	
+document.getElementById('ul1expLab').onclick = function() {
+	window.open( "experiencia-laboral.html","Experiencia laboral","width=700px, height=410px")};
+	
+document.getElementById('ul1habilidades').onclick = function() {
+	window.open( "habilidades.html","Habilidades","width=700px, height=450px")};
+
+document.getElementById('ul1contacto').onclick = function() {
+	window.open( "form-contacto.html","Contactar")};
+	
+document.getElementById('ul5-contacto').onclick = function() {
+	window.open( "form-contacto.html","Contacto")};
+
+
+document.getElementById('logoInstagram').onclick = function() {
+	window.open( "https://www.instagram.com/","Instagram")};	 
+	
+
+document.getElementById('logoTwitter').onclick = function() {
+	window.open( "https://twitter.com/","Twitter")};
+	
+document.getElementById('logoFacebook').onclick = function() {
+	window.open( "https://es-la.facebook.com/","Facebook")};	
